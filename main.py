679ca0a8cc9a1f9483b6779347864ce51fc6493c
laisiewcheng <22535339+laisiewcheng@users.noreply.github.com>
2020-06-08 15:29:57 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ parser.add_argument('--resume', default='', type=str, metavar='PATH',
 #parser.add_argument('--resume', '-r', action = 'store_true', help = 'resume from checkpoint')
 parser.add_argument('-j', '--workers', default=0, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
-parser.add_argument('-b', '--batch-size', default=128, type=int,
+parser.add_argument('-b', '--batch-size', default=64, type=int,
                     metavar='N', help='mini-batch size (default: 128)')
 parser.add_argument('--print-freq', '-p', default=50, type=int,
                     metavar='N', help='print frequency (default: 20)')
